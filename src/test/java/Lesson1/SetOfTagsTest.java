712e6fa66db4b52296cb52763c88d8cf1ae943d0
Александр Сысоев <APSysoev@sberbank.ru>
2021-03-16 23:57:03 +0300

    Пустой SetOfTagsTest

---
+++ src/test/java/Lesson1/SetOfTagsTest.java
@@ -0,0 +1,56 @@
+package Lesson1;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class SetOfTagsTest {
+
+    static SetOfTags tags;
+
+    @Before
+    public void setUp() {
+        tags = new SetOfTags();
+    }
+
+    @Test
+    public void testToString() {
+    }
+
+    @Test
+    public void isEmpty() {
+    }
+
+    @Test
+    public void size() {
+    }
+
+    @Test
+    public void clear() {
+    }
+
+    @Test
+    public void add() {
+    }
+
+    @Test
+    public void contains() {
+    }
+
+    @Test
+    public void remove() {
+    }
+
+    @Test
+    public void union() {
+    }
+
+    @Test
+    public void difference() {
+    }
+
+    @Test
+    public void intersection() {
+    }
+}
